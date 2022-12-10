ca2b0c96a5f894f15d1d41d8c3d763fef7c958ee
hovercao <17388766232@163.com>
2022-12-10 17:40:41 +0800

    feat:针对单次请求的拦截器

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -24,21 +24,31 @@ class Request {
     });
 
     //添加自定义拦截器
-    config.interceptors?.requestSuccessFn &&
     this.instance.interceptors.request.use(
-      config.interceptors.requestSuccessFn,
-      config.interceptors.requestFailureFn,
+      config.interceptors?.requestSuccessFn,
+      config.interceptors?.requestFailureFn,
     );
 
-    config.interceptors?.responseSuccessFn &&
     this.instance.interceptors.response.use(
-      config.interceptors.responseSuccessFn,
-      config.interceptors.responseFailureFn,
+      config.interceptors?.responseSuccessFn,
+      config.interceptors?.responseFailureFn,
     );
   }
 
-  request(config: AxiosRequestConfig) {
-    return this.instance.request(config);
+  request(config: devAxiosRequestConfig) {
+    if (config.interceptors?.requestSuccessFn) {
+      config = config.interceptors?.requestSuccessFn(config);
+    }
+    return new Promise((resolve, reject) => {
+      this.instance.request(config).then(res => {
+        if (config.interceptors?.responseSuccessFn) {
+          res = config.interceptors.responseSuccessFn(res);
+        }
+        resolve(res);
+      }).catch(error => {
+        reject(error);
+      });
+    });
   }
 }
 
