df321f587d7200ddd0be2dd17be571da35e0c46f
hovercao <17388766232@163.com>
2022-12-13 18:27:13 +0800

    fix:解决二级路由加载屏幕闪烁

--- src/views/Discover/index.tsx
+++ src/views/Discover/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
 
 const Discover = () => {
   return (
     <>
-      Discover
-      <Outlet />
+      <Link to={"/discover/recommend"}>推荐</Link>
+      <Link to={"/discover/ranking"}>排行榜</Link>
+      <Link to={"/discover/songs"}>歌单</Link>
+      <Link to={"/discover/djRadio"}>主播电台</Link>
+      <Link to={"/discover/artist"}>歌手</Link>
+      <Link to={"/discover/album"}>新碟上架</Link>
+      <Suspense fallback={"加载中"}>
+        <Outlet />
+      </Suspense>
     </>
   );
 };
