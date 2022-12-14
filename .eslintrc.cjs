6fbcd5f8e16b74e7c368800bd57f6e2594bde8f4
hovercao <17388766232@163.com>
2022-12-14 11:12:37 +0800

    styled:换行符修改

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,6 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
-    endOfLine: "off"
+    "endOfLine": "off"
   }
 };
