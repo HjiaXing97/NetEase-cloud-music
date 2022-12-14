20f486403df606abff41234593b550bed388f811
hovercao <17388766232@163.com>
2022-12-14 10:18:43 +0800

    styled:全局格式化代码

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
   plugins: ["react", "@typescript-eslint"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off"
+    "@typescript-eslint/no-unused-vars": "off",
+    endOfLine: "off"
   }
 };
